4cb270d6fadb3111398b9d52748f95a26f8ecd93
Sebastian B. Knapp <rock@ccls-online.de>
2012-09-21 01:33:34 +0200

    document test no. 1

---
+++ t/html5.c
@@ -0,0 +1,21 @@
+
+
+#include <wisper/html5.h>
+#include <tap.h>
+
+static void check_clean_doc()
+{
+   text_t result = new_text();
+   html5_document_t doc = new_html5_core_document();
+   
+   char* expect =
+      "<?doctype html?><html><head></head><body></body></html>";
+   render_node(doc->document,result);
+   is(text_get_text(result),expect,"clean document");
+   delete_html5_document(doc); 
+}
+
+int main()
+{
+   check_clean_doc();
+}
