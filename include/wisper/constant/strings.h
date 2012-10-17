8cb0aa5a85e994c6b0a5de8f9dedf093a8efab71
Sebastian B. Knapp <rock@ccls-online.de>
2012-10-17 09:39:47 +0200

    constant strings

---
+++ include/wisper/constant/strings.h
@@ -0,0 +1,8 @@
+
+#ifndef _WISPER_CONSTANT_STRINGS_H_
+# define _WISPER_CONSTANT_STRINGS_H_
+
+extern const char const* constant_str_space;
+extern const char const* constant_str_newline;
+
+#endif
---
+++ src/constant_strings.c
@@ -0,0 +1,7 @@
+
+#include <wisper/constant/strings.h>
+
+
+const char const* constant_str_newline = "\n";
+const char const* constant_str_space = " ";
+
