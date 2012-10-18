709440a7462906e92aff0573f791bdf88f3370b0
Sebastian B. Knapp <rock@ccls-online.de>
2012-10-18 21:11:22 +0200

    is - testfunction improved

--- src/tap.c
+++ src/tap.c
@@ -1,6 +1,7 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include <tap.h>
 
 bool ok(bool condition, const char *msg)
@@ -16,7 +17,19 @@ bool ok(bool condition, const char *msg)
 
 bool is(const char* arg0, const char* arg1, const char* msg)
 {
-  return ok(0 == strcmp(arg0,arg1),msg);	
+  bool result = (0 == strcmp(arg0,arg1));
+  if(result) {
+    return ok(result,msg);
+  }
+  else {
+	char* moddle = " is not ";
+	char* errmsg = (char*) malloc(
+		strlen(arg0) + strlen(moddle) + strlen(arg1) + 5);
+	sprintf(errmsg,"´%s´%s´%s´",arg0,moddle,arg1);
+	ok(result,msg);
+	diag(errmsg);
+	return result;
+  }	
 }
 
 void diag(const char* msg)
